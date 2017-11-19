ab7ee702ed9e4f91bc281f1e423a859d31aa2749
LeftShiftJones <jonesr1@mail.gvsu.edu>
2017-11-18 22:02:08 -0500

    Added Trees

---
+++ Trees/KDTree/KDTree.java
@@ -0,0 +1,15 @@
+/**
+Class of the KD Tree. An elusive creature that allows the storage of 2-Dimensional data in a 1-dimensional format.
+
+*/
+
+public class KDTree {
+
+    /**  */
+    private int size;
+
+    public KDTree() {
+
+    }
+
+}
