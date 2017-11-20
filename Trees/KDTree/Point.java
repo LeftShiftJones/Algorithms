6abe31f0c1a4dddc546b80b871051419743a8008
LeftShiftJones <jonesr1@mail.gvsu.edu>
2017-11-19 23:52:11 -0500

    Deleted the html file I accidentally put in here. Added some stuff.

--- Trees/KDTree/Point.java
+++ Trees/KDTree/Point.java
@@ -5,9 +5,43 @@ public class Point {
         this.x = 0.0;
         this.y = 0.0;
     }
- 
+
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Point add(double x, double y) {
+        return new Point(this.x + x, this.y + y);
+    }
+
+    /**
+    Get x-value of point
+    @return x-value
+    */
+    public double x() {
+        return this.x;
+    }
+
+    /**
+    Get x-value of point
+    @return y-value
+    */
+    public double y() {
+        return this.y;
+    }
+
+    /**
+    Returns a squared geometric distance between two points
+    */
+    public double distSquared(Point p) {
+        return (Math.pow(this.x - p.x(), 2) + Math.pow(this.y - p.y(), 2));
+    }
+
+    /**
+    Returns a simplified geometric distance between two points
+    */
+    public double dist(Point p) {
+        return Math.sqrt(distSquared(p));
     }
 }
