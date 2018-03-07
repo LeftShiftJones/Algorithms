cbaf6444a7aa28039544e3234b8ccb9cdfad99e8
LeftShiftJones <jonesr1@mail.gvsu.edu>
2018-03-07 00:01:19 -0500

    Moved number generation to decrease runtime

--- GetToZero.java
+++ GetToZero.java
@@ -3,20 +3,21 @@
     operations (+1, -1 when odd; /2 when even)
     does it take to get from some positive #
     down to 1.
-    Written for COS 320: Algorithm Analysis
 
+    Written for COS 320: Algorithm Analysis
     @version 1.0
     Date: 03/02/2018
     @author Ryan Jones
 */
+
 import java.util.Scanner;
 
-public class GetToZero {
+public class FooBar {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a number to reduce: ");
         int i = scan.nextInt();
-        runGetToZero(i);
+        runFooBar(i);
     }
 
     /**
@@ -24,24 +25,27 @@ public class GetToZero {
         to get to zero as fast as
         possible
     */
-    private static void runGetToZero(int i) {
+    private static int runFooBar(int i) {
         int count = 0;
+        int i2 = pwr2(i), i2div;
+        int hi = (int)Math.pow(2,30);
         while (i > 1) {
             System.out.println(i);
             //odd case
             if(i%2 == 1){
-                int i2 = pwr2(i), i2div = i2/2;
-                int hi = (int)Math.pow(2,30);
+                i2div = i2/2;
                 //check overflow case
                 if(i2 == hi) i = ((i - hi) < i2div || i==2147483647) ?  i-1 : i+1;
-                else i = ( (i - i2) < i2div || (i-1==i2) ) ? i-1 : i+1;
-                count++;
-                continue;
+                else i = ((i - i2) < i2div || (i-1==i2)) ? i-1 : i+1;
             }
-            i /= 2;
+            else {
+                i  /= 2;
+                i2 /= 2;
+            }
             count++;
         }
-        System.out.println(i+"\n\n"+count + " operations to get to 1.");
+        System.out.println(count + " operations to get to 1.");
+        return count;
     }
 
     /**
@@ -53,7 +57,7 @@ public class GetToZero {
         int i = 1;
         do {
             i += i;
-        }while (i < n/2);
+        }while (i != Math.pow(2, 30) && i*2 < n);
         return i;
     }
 }
